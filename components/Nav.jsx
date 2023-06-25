db8fc19fe8b17dbbce4c5bf12104976dfb2fb47e
thejakia1 <thejakia1@gmail.com>
2023-06-25 12:44:48 +0600

    authentication part done

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,20 +6,17 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
-            const response = await getProviders();
-
-            setProviders(response);
-        }
-
-        setProviders();
-    }, [])
+        (async () => {
+            const res = await getProviders();
+            setProviders(res);
+        })();
+    }, []);
 
 
 
@@ -39,7 +36,7 @@ const Nav = () => {
             {/* Desktop Navigation */}
 
             <div className="sm:flex hidden">
-                {isUserLoggedIn ?
+                {session?.user ?
                     <div className="flex gap-3 md:gap-5">
                         <Link href="/create-prompt" className="black_btn">
                             Create Post
@@ -51,7 +48,7 @@ const Nav = () => {
 
                         <Link href="/profile">
                             <Image
-                                src="/assets/images/logo.svg"
+                                src={session?.user.image}
                                 width={37}
                                 height={37}
                                 className="rounded-full"
@@ -77,10 +74,10 @@ const Nav = () => {
 
             {/* Mobile Navigation  */}
             <div className="sm:hidden flex relative">
-                {isUserLoggedIn ? (
+                {session?.user ? (
                     <div className="flex">
                         <Image
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className="rounded-full"
@@ -106,7 +103,7 @@ const Nav = () => {
                                 </Link>
                                 <button
                                     type="button"
-                                    onClick={() => { setToggleDropdown(false); signOut();}}
+                                    onClick={() => { setToggleDropdown(false); signOut(); }}
                                     className="mt-5 w-full black_btn"
                                 >
                                     Sign Out
